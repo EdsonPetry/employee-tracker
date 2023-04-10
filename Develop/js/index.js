27b55e5b5fe57ee65dcbf52d0c25efb82ad665a3
Edson Petry <edsonlbpetry@gmail.com>
2023-04-09 23:38:17 -0400

    setup complete

--- Develop/js/index.js
+++ Develop/js/index.js
@@ -4,7 +4,7 @@ const queries = require('./queries');
 // Main menu prompt
 function mainMenu() {
   inquirer.prompt([
-    // Add your prompt questions for the main menu here
+    // Add prompt questions for the main menu here
   ]).then((answers) => {
     // Handle the user's choice here
   });
